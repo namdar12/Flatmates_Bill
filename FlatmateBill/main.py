7671a71ce7d815721b0e436add01a8bebf955a15
Namdar Mesri <namdarmesri@Namdars-MacBook-Pro.local>
2021-10-17 13:33:53 -0700

    first commit

---
+++ FlatmateBill/main.py
@@ -0,0 +1,44 @@
+class Bill:
+    """
+    Object that contains data about a bill, such as
+    total amount and period of the bill
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Creates a flatmate person who lives in the flat
+    and pays a share of the bill
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill):
+        return bill.amount/2
+
+
+class PdfReport:
+    """
+    Creates a pdf file that contains data about the flatmates
+    such as their names, their due amount, and the period of the bill
+    """
+
+    def __init__(self,filename):
+        self.filename = filename
+
+    def generate(self,flatmate1,flatmate2,bill):
+        pass
+
+
+
+electric_bill = Bill(120,"March 2020")
+John = Flatmate("John",20)
+Smith = Flatmate("Smith",20)
+
+print(John.pays(electric_bill))
