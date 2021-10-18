52eee2c4a38f2362c576d78ef8820b711f3b7102
namdar12 <namdar.mesri12@gmail.com>
2021-10-17 20:37:24 -0700

    Generalized amount of flatmates from 2 to n

--- main.py
+++ main.py
@@ -22,8 +22,12 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill, flatmate2):
-        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+    def pays(self, bill, *args):
+        total_days = 0
+        for item in args:
+            item.days_in_house
+            total_days = item.days_in_house + total_days
+        weight = self.days_in_house / (total_days)
         to_pay = bill.amount * float(weight)
         return to_pay
 
@@ -37,7 +41,7 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def generate(self, flatmate1, flatmate2, bill):
+    def generate(self, *args, bill):
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
@@ -50,11 +54,9 @@ class PdfReport:
         pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
 
         #Insert the name and pay amount for each flatmate
-        pdf.cell(w=100,h=40,txt=flatmate1.name, border =1)
-        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill,flatmate2)), border=1, ln=1)
-
-        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=150, h=40, txt=str(flatmate2.pays(bill, flatmate1)), border=1, ln=1)
+        for flat in args:
+            pdf.cell(w=100,h=40,txt=flat.name, border =1)
+            pdf.cell(w=150, h=40, txt=str(flat.pays(bill,*args)), border=1, ln=1)
 
         pdf.output(self.filename + '.pdf')
 
@@ -62,11 +64,15 @@ class PdfReport:
 electric_bill = Bill(120, "March 2020")
 John = Flatmate("John", 20)
 Smith = Flatmate("Smith", 25)
+Jane = Flatmate("Jane",20)
 
-print(John.pays(electric_bill, flatmate2=Smith))
-print(Smith.pays(electric_bill, flatmate2=John))
+print(John.pays(electric_bill,Smith,John,Jane))
+print(Smith.pays(electric_bill,Smith,John,Jane))
+print(Jane.pays(electric_bill,Smith,John,Jane))
+
+teddy = John.pays(electric_bill,Smith,John,Jane)+Smith.pays(electric_bill,Smith,John,Jane)+Jane.pays(electric_bill,Smith,John,Jane)
 
 bill_pdf = PdfReport("Bill_report")
 
-bill_pdf.generate(John,Smith,electric_bill)
+bill_pdf.generate(John,Smith,Jane, bill = electric_bill)
 
