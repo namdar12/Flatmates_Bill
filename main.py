673002f380c733a47b2ab4343318fe56d5b91cb1
namdar12 <namdar.mesri12@gmail.com>
2021-10-17 17:47:13 -0700

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as
@@ -19,8 +22,10 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        return bill.amount/2
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * float(weight)
+        return to_pay
 
 
 class PdfReport:
@@ -29,16 +34,39 @@ class PdfReport:
     such as their names, their due amount, and the period of the bill
     """
 
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.filename = filename
 
-    def generate(self,flatmate1,flatmate2,bill):
-        pass
+    def generate(self, flatmate1, flatmate2, bill):
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='c', ln=1)
+
+        # Insert period label and values
+        pdf.cell(w=100, h=40, txt="Period: ", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+        #Insert the name and pay amount for each flatmate
+        pdf.cell(w=100,h=40,txt=flatmate1.name, border =1)
+        pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill,flatmate2)), border=1, ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=150, h=40, txt=str(flatmate2.pays(bill, flatmate1)), border=1, ln=1)
+
+        pdf.output(self.filename + '.pdf')
 
 
+electric_bill = Bill(120, "March 2020")
+John = Flatmate("John", 20)
+Smith = Flatmate("Smith", 25)
 
-electric_bill = Bill(120,"March 2020")
-John = Flatmate("John",20)
-Smith = Flatmate("Smith",20)
+print(John.pays(electric_bill, flatmate2=Smith))
+print(Smith.pays(electric_bill, flatmate2=John))
 
-print(John.pays(electric_bill))
+bill_pdf = PdfReport("Bill_report")
+
+bill_pdf.generate(John,Smith,electric_bill)
+
