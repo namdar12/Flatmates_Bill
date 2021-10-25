1570ab1e84ae22cf58efdcad5fc018594bc7b453
namdar12 <namdar.mesri12@gmail.com>
2021-10-24 19:13:52 -0700

    Created a CLI

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import webbrowser
+
 from fpdf import FPDF
 
 
@@ -47,32 +50,55 @@ class PdfReport:
 
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='c', ln=1)
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=0, align='c', ln=1)
 
         # Insert period label and values
-        pdf.cell(w=100, h=40, txt="Period: ", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt="Period: ", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         #Insert the name and pay amount for each flatmate
         for flat in args:
-            pdf.cell(w=100,h=40,txt=flat.name, border =1)
-            pdf.cell(w=150, h=40, txt=str(flat.pays(bill,*args)), border=1, ln=1)
+            pdf.set_font(family='Times', size=14, style='I')
+            pdf.cell(w=100,h=40,txt=flat.name, border =0)
+            pdf.cell(w=150, h=40, txt=str(flat.pays(bill,*args)), border=0, ln=1)
 
         pdf.output(self.filename + '.pdf')
+        webbrowser.open('file://' + os.path.realpath(self.filename))
 
 
-electric_bill = Bill(120, "March 2020")
-John = Flatmate("John", 20)
-Smith = Flatmate("Smith", 25)
-Jane = Flatmate("Jane",20)
+# electric_bill = Bill(120, "March 2020")
+# John = Flatmate("John", 20)
+# Smith = Flatmate("Smith", 25)
+# Jane = Flatmate("Jane",20)
+#
+# print(John.pays(electric_bill,Smith,John,Jane))
+# print(Smith.pays(electric_bill,Smith,John,Jane))
+# print(Jane.pays(electric_bill,Smith,John,Jane))
+#
+# teddy = John.pays(electric_bill,Smith,John,Jane)+Smith.pays(electric_bill,Smith,John,Jane)+Jane.pays(electric_bill,Smith,John,Jane)
+#
+# bill_pdf = PdfReport("Bill_report")
+#
+# bill_pdf.generate(John,Smith,Jane, bill = electric_bill)
 
-print(John.pays(electric_bill,Smith,John,Jane))
-print(Smith.pays(electric_bill,Smith,John,Jane))
-print(Jane.pays(electric_bill,Smith,John,Jane))
+#Number of users
+date = input("Please enter the month: ")
+bill_cost = float(input("Please enter the bill cost: "))
+bill = Bill(bill_cost,date)
 
-teddy = John.pays(electric_bill,Smith,John,Jane)+Smith.pays(electric_bill,Smith,John,Jane)+Jane.pays(electric_bill,Smith,John,Jane)
+list1 = []
+count = 0
+while count < 4:
+    try:
+        Flatmate_p, Flatmate_days = input("Enter a two value: ").split()
+        Flatmate_object = Flatmate(Flatmate_p,Flatmate_days)
+        list1.append(Flatmate_object)
+        count += 1
+    except:
+        print("FIRE")
 
-bill_pdf = PdfReport("Bill_report")
 
-bill_pdf.generate(John,Smith,Jane, bill = electric_bill)
+for user in list1:
+    print(user.name)
+
 
